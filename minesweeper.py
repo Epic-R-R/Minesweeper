e396607d037ecdf92537ed4b5bd131b25c424cf8
Epic-R-R <epicrr001@gmail.com>
2020-12-20 21:40:52 +0330

    Function set mines

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,5 @@
+import random
+
 # Minesweeper Layout
 def mines_layout():
     global mine_values
@@ -34,6 +36,29 @@ def mines_layout():
         print(st + '|')
  
     print()
+
+# setting up Mines
+def set_mines():
+ 
+    global numbers
+    global mines_no
+    global n
+ 
+    # Track of number of mines already set up
+    count = 0
+    while count < mines_no:
+ 
+        # Random number from all possible grid positions 
+        val = random.randint(0, n*n-1)
+ 
+        # Generating row and column from the number
+        r = val // n
+        col = val % n
+ 
+        # Place the mine, if it doesn't already have one
+        if numbers[r][col] != -1:
+            count = count + 1
+            numbers[r][col] = -1
 
 
 if __name__ == "__main__":
