9ac27e81f5fdad7792236a1f386182425f72d88c
Epic-R-R <epicrr001@gmail.com>
2020-12-20 21:41:53 +0330

    set values func

--- minesweeper.py
+++ minesweeper.py
@@ -60,6 +60,45 @@ def set_mines():
             count = count + 1
             numbers[r][col] = -1
 
+# Setting up the other grid values
+def set_values():
+ 
+    global numbers
+    global n
+ 
+    # Loop for counting each cell value
+    for r in range(n):
+        for col in range(n):
+ 
+            # Skip, if it contains a mine
+            if numbers[r][col] == -1:
+                continue
+ 
+            # Check up  
+            if r > 0 and numbers[r-1][col] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check down    
+            if r < n-1  and numbers[r+1][col] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check left
+            if col > 0 and numbers[r][col-1] == -1:
+                numbers[r] = numbers[r] + 1
+            # Check right
+            if col < n-1 and numbers[r][col+1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check top-left    
+            if r > 0 and col > 0 and numbers[r-1][col-1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check top-right
+            if r > 0 and col < n-1 and numbers[r-1][col+1]== -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check below-left  
+            if r < n-1 and col > 0 and numbers[r+1][col-1]== -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check below-right
+            if r < n-1 and col< n-1 and numbers[r+1][col+1]==-1:
+                numbers[r][col] = numbers[r][col] + 1
+
 
 if __name__ == "__main__":
  
