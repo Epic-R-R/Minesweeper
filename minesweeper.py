24d89635af0d5ff404b30c5e635b9a48cb7128ad
Epic-R-R <epicrr001@gmail.com>
2020-12-20 21:54:38 +0330

    beautiful code

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,15 @@
+# Importing packages
 import random
-
-# Minesweeper Layout
-def mines_layout():
+import os
+ 
+# Printing the Minesweeper Layout
+def print_mines_layout():
+ 
     global mine_values
     global n
  
     print()
-    print("\t\t\tMinesweeper\n")
+    print("\t\t\tMINESWEEPER\n")
  
     st = "   "
     for i in range(n):
@@ -36,8 +39,8 @@ def mines_layout():
         print(st + '|')
  
     print()
-
-# setting up Mines
+  
+# Function for setting up Mines
 def set_mines():
  
     global numbers
@@ -59,8 +62,8 @@ def set_mines():
         if numbers[r][col] != -1:
             count = count + 1
             numbers[r][col] = -1
-
-# Setting up the other grid values
+ 
+# Function for setting up the other grid values
 def set_values():
  
     global numbers
@@ -82,7 +85,7 @@ def set_values():
                 numbers[r][col] = numbers[r][col] + 1
             # Check left
             if col > 0 and numbers[r][col-1] == -1:
-                numbers[r] = numbers[r] + 1
+                numbers[r][col] = numbers[r][col] + 1
             # Check right
             if col < n-1 and numbers[r][col+1] == -1:
                 numbers[r][col] = numbers[r][col] + 1
@@ -90,27 +93,16 @@ def set_values():
             if r > 0 and col > 0 and numbers[r-1][col-1] == -1:
                 numbers[r][col] = numbers[r][col] + 1
             # Check top-right
-            if r > 0 and col < n-1 and numbers[r-1][col+1]== -1:
+            if r > 0 and col < n-1 and numbers[r-1][col+1] == -1:
                 numbers[r][col] = numbers[r][col] + 1
             # Check below-left  
-            if r < n-1 and col > 0 and numbers[r+1][col-1]== -1:
+            if r < n-1 and col > 0 and numbers[r+1][col-1] == -1:
                 numbers[r][col] = numbers[r][col] + 1
             # Check below-right
-            if r < n-1 and col< n-1 and numbers[r+1][col+1]==-1:
+            if r < n-1 and col < n-1 and numbers[r+1][col+1] == -1:
                 numbers[r][col] = numbers[r][col] + 1
-
-# Show mines
-def show_mines():
-    global mine_values
-    global numbers
-    global n
  
-    for r in range(n):
-        for col in range(n):
-            if numbers[r][col] == -1:
-                mine_values[r][col] = 'M'
-
-
+# Recursive function to display all zero-valued neighbours  
 def neighbours(r, col):
      
     global mine_values
@@ -146,12 +138,21 @@ def neighbours(r, col):
                 neighbours(r+1, col-1)
             if r < n-1 and col < n-1:
                 neighbours(r+1, col+1)  
-                 
+ 
         # If the cell is not zero-valued            
         if numbers[r][col] != 0:
                 mine_values[r][col] = numbers[r][col]
-
-
+ 
+# Function for clearing the terminal
+def clear():
+    os.system("clear")      
+ 
+# Function to display the instructions
+def instructions():
+    print("Instructions:")
+    print("1. Enter row and column number to select a cell, Example \"2 3\"")
+    print("2. In order to flag a mine, enter F after row and column numbers, Example \"2 3 F\"")
+ 
 # Function to check for completion of the game
 def check_over():
     global mine_values
@@ -174,42 +175,55 @@ def check_over():
         return True
     else:
         return False
-        
+ 
+# Display all the mine locations                    
+def show_mines():
+    global mine_values
+    global numbers
+    global n
+ 
+    for r in range(n):
+        for col in range(n):
+            if numbers[r][col] == -1:
+                mine_values[r][col] = 'M'
+ 
+ 
 if __name__ == "__main__":
  
     # Size of grid
     n = 8
     # Number of mines
     mines_no = 8
-    
+ 
     # The actual values of the grid
     numbers = [[0 for y in range(n)] for x in range(n)] 
     # The apparent values of the grid
     mine_values = [[' ' for y in range(n)] for x in range(n)]
     # The positions that have been flagged
     flags = []
-
+ 
     # Set the mines
     set_mines()
-    
+ 
     # Set the values
     set_values()
-    
+ 
     # Display the instructions
     instructions()
-    
+ 
     # Variable for maintaining Game Loop
     over = False
-    
+         
     # The GAME LOOP 
     while not over:
         print_mines_layout()
+ 
         # Input from the user
         inp = input("Enter row number followed by space and column number = ").split()
-
-        # Standard Move
+         
+        # Standard input
         if len(inp) == 2:
-        
+ 
             # Try block to handle errant input
             try: 
                 val = list(map(int, inp))
@@ -218,15 +232,15 @@ if __name__ == "__main__":
                 print("Wrong input!")
                 instructions()
                 continue
-        
-        # Flag Input
+ 
+        # Flag input
         elif len(inp) == 3:
             if inp[2] != 'F' and inp[2] != 'f':
                 clear()
                 print("Wrong Input!")
                 instructions()
                 continue
-        
+ 
             # Try block to handle errant input  
             try:
                 val = list(map(int, inp[:2]))
@@ -235,46 +249,68 @@ if __name__ == "__main__":
                 print("Wrong input!")
                 instructions()
                 continue
-        
-            # Sanity checks
+ 
+            # Sanity checks 
             if val[0] > n or val[0] < 1 or val[1] > n or val[1] < 1:
                 clear()
-                print("Wrong Input!")
+                print("Wrong input!")
                 instructions()
                 continue
-            
+ 
             # Get row and column numbers
             r = val[0]-1
-            col = val[1]-1
-
+            col = val[1]-1 
+ 
             # If cell already been flagged
             if [r, col] in flags:
                 clear()
                 print("Flag already set")
                 continue
-            
+ 
             # If cell already been displayed
             if mine_values[r][col] != ' ':
                 clear()
                 print("Value already known")
                 continue
-            
+ 
             # Check the number for flags    
             if len(flags) < mines_no:
                 clear()
                 print("Flag set")
-            
+ 
                 # Adding flag to the list
                 flags.append([r, col])
-                
+                 
                 # Set the flag for display
                 mine_values[r][col] = 'F'
                 continue
             else:
                 clear()
                 print("Flags finished")
-                continue
-
+                continue    
+ 
+        else: 
+            clear()
+            print("Wrong input!")   
+            instructions()
+            continue
+             
+ 
+        # Sanity checks
+        if val[0] > n or val[0] < 1 or val[1] > n or val[1] < 1:
+            clear()
+            print("Wrong Input!")
+            instructions()
+            continue
+             
+        # Get row and column number
+        r = val[0]-1
+        col = val[1]-1
+ 
+        # Unflag the cell if already flagged
+        if [r, col] in flags:
+            flags.remove([r, col])
+ 
         # If landing on a mine --- GAME OVER    
         if numbers[r][col] == -1:
             mine_values[r][col] = 'M'
@@ -283,4 +319,22 @@ if __name__ == "__main__":
             print("Landed on a mine. GAME OVER!!!!!")
             over = True
             continue
-        
+ 
+        # If landing on a cell with 0 mines in neighboring cells
+        elif numbers[r][col] == 0:
+            vis = []
+            mine_values[r][col] = '0'
+            neighbours(r, col)
+ 
+        # If selecting a cell with atleast 1 mine in neighboring cells  
+        else:   
+            mine_values[r][col] = numbers[r][col]
+ 
+        # Check for game completion 
+        if(check_over()):
+            show_mines()
+            print_mines_layout()
+            print("Congratulations!!! YOU WIN")
+            over = True
+            continue
+        clear()
