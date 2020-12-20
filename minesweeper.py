e455f20348b74d59e903fdf63d820dcaada62d3e
Epic-R-R <epicrr001@gmail.com>
2020-12-20 21:39:18 +0330

    __name__ : __main__

--- minesweeper.py
+++ minesweeper.py
@@ -34,3 +34,18 @@ def mines_layout():
         print(st + '|')
  
     print()
+
+
+if __name__ == "__main__":
+ 
+    # Size of grid
+    n = 8
+    # Number of mines
+    mines_no = 8
+ 
+    # The actual values of the grid
+    numbers = [[0 for y in range(n)] for x in range(n)] 
+    # The apparent values of the grid
+    mine_values = [[' ' for y in range(n)] for x in range(n)]
+    # The positions that have been flagged
+    flags = []
